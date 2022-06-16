b9f07284d2a0458b3e0885aef4c2f5dd1b207604
Christian Toledo <chrstiantoledo@gmail.com>
2022-06-16 19:31:05 -0300

    chore: move pages directory to src

--- src/pages/home.spec.tsx
+++ src/pages/home.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Home from './index'
-import '@testing-library/jest-dom'
+import '@testing-library/dom'
 
 describe('Home', () => {
   it('renders a heading', () => {
